1c3ce6ac75ad2e0f04b2d86926c0882cee788d71
Jesus Milan <jmilan.gonzalez@gmail.com>
2020-12-27 20:54:01 -0500

    commit before trying else if for password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,9 @@
 // Prompt welcome screen
 alert("Welcome User Let's Make A New Password For You!");
-var passwordLength = prompt("How many characters would you like your new password to have?");
-var passwordCapital = confirm("Would you like it to have capital letters?");
-var passwordNumber = confirm("Would you like it to have numbers?");
-var passwordSecial = confirm("Would you like it to have special characters?");
-
+var newPassword = "abcdefghijklmnopqrstuvwxyz";
 // Alerts showing user input
 // How many characters does the user want the password to include
+var passwordLength = prompt("How many characters would you like your new password to have?");
 if (passwordLength < 8 || passwordLength > 128){
   alert("Your new password must be between 8 and 128 characters long.")
 }
@@ -14,28 +11,36 @@ else{
   alert("OK your new password will be " + passwordLength + " characters long!");
 }
 
-// SHows whether the user wants capital letters
+// Shows whether the user wants capital letters
+var passwordCapital = confirm("Would you like it to have capital letters?");
 if (passwordCapital) {
   alert("OK your new password will have capital letters!");
+  newPassword = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 }
 else {
   alert("Your new password will NOT have capital letters!");
 }
 
 // Shows whether the user wants numbers in the password
+var passwordNumber = confirm("Would you like it to have numbers?");
 if (passwordNumber){
   alert("OK your new password will have numbers!");
+  newPassword = passwordCapital + "0123456789";
 }
 else {
   alert("Your new password will NOT have numbers!");
+  newPassword = passwordCapital;
 }
 
 //  Shows whether the user wants special characters on their password
+var passwordSecial = confirm("Would you like it to have special characters?");
 if (passwordSecial){
   alert("OK your new password will have special characters!");
+  newPassword = passwordCapital + "!@#$%^&*()?";
 }
 else {
   alert("Your new password will NOT have special characters!");
+  newPassword = passwordCapital;
 }
 
 // Assignment Code
@@ -52,7 +57,7 @@ function writePassword() {
 
 function generatePassword() {
   var length = passwordLength,
-      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      charset = newPassword,
       retVal = "";
   for (var i = 0, n = charset.length; i < length; ++i) {
       retVal += charset.charAt(Math.floor(Math.random() * n));
